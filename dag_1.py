8802b526d9a7b27796354a9e1b44592586e2e769
Sreekesh S <sreekesh.s@thoughtworks.com>
2020-04-15 12:37:21 +0530

    adds dag

---
+++ dag_1.py
@@ -0,0 +1,40 @@
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+def greet():
+    print('Writing in file')
+    with open('path/to/file/greet.txt', 'a+', encoding='utf8') as f:
+        now = dt.datetime.now()
+        t = now.strftime("%Y-%m-%d %H:%M")
+        f.write(str(t) + '\n')
+    return 'Greeted'
+def respond():
+    return 'Greet Responded Again'
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': dt.datetime(2020, 2, 21, 13, 25, 00),
+    'concurrency': 1,
+    'retries': 0
+}
+
+with DAG('my_simple_dag',
+         default_args=default_args,
+         schedule_interval='*/60 * * * *',
+         ) as dag:
+    opr_hello = BashOperator(task_id='say_Hi',
+                             bash_command='echo "Hi!!"')
+
+    opr_greet = PythonOperator(task_id='greet',
+                               python_callable=greet)
+    opr_sleep = BashOperator(task_id='sleep_me',
+                             bash_command='sleep 1')
+
+    opr_respond = PythonOperator(task_id='respond',
+                                 python_callable=respond)
+opr_hello >> opr_greet >> opr_sleep >> opr_respond
+
